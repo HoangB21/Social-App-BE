e87db61d94a07e761fbf9d841d91ac26fc2364fc
HoangB21 <tonghoang213@gmail.com>
2025-10-06 15:32:08 +0700

    fix cors

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: process.env.ALLOWED_ORIGINS.split(","),
   })
 );
 app.use(cookieParser());
