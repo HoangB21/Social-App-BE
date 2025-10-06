13e24796ed43862fb139cc1d128a0bd4399b8e28
HoangB21 <tonghoang213@gmail.com>
2025-10-06 12:04:32 +0700

    fix cors

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "http://13.239.38.18",
   })
 );
 app.use(cookieParser());
