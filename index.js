02008027790f5434e34f49bcc585aac800c9bfc5
HoangB21 <tonghoang213@gmail.com>
2025-10-09 00:34:54 +0700

    fix cors

--- index.js
+++ index.js
@@ -16,16 +16,18 @@ import dotenv from "dotenv";
 dotenv.config();
 
 //middlewares
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Credentials", true);
-  next();
-});
-app.use(express.json());
 app.use(
   cors({
     origin: process.env.ALLOWED_ORIGINS.split(","),
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   })
 );
+
+app.options("*", cors());
+
+app.use(express.json());
 app.use(cookieParser());
 
 const s3 = new S3Client({
